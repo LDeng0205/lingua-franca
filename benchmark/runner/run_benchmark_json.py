0feeb5a3a464fc8b5e022aff08425da5db945f61
LDeng0205 <arthurdeng0205@hotmail.com>
2022-03-01 15:51:07 -0800

    fixed bug in division

--- benchmark/runner/run_benchmark_json.py
+++ benchmark/runner/run_benchmark_json.py
@@ -181,7 +181,7 @@ def write_results(times, cfg):
     total_time = 0
     for t in times:
         total_time += t
-    total_time /= len(times)
+    total_time /= cfg["iterations"]
     data = {
             "name": cfg["benchmark"]["name"],
             "unit": "ms",
